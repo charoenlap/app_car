85c608a2af20897319549c197f5b9243b31efc78
obiwan-overflow <kittipopp@hotmail.co.th>
2021-01-28 13:38:26 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
-
+  
+	brands = [
+		{img: 'assets/images/logo/honda-logo-1700x1150.png'},
+		{img: 'assets/images/logo/Mitsubishi-logo-2000x2500.png'},
+		{img: 'assets/images/logo/Mercedes-Benz-logo-2011-1920x1080.png'},
+		{img: 'assets/images/logo/lamborghini-logo-1000x1100.png'},
+		{img: 'assets/images/logo/Suzuki-logo-5000x2500.png'},
+		{img: 'assets/images/logo/Chevrolet-logo-2013-2560x1440.png'},
+		{img: 'assets/images/logo/ford-logo-2017.png'},
+		{img: 'assets/images/logo/Jeep-logo-green-3840x2160.png'},
+		{img: 'assets/images/logo/Kia-logo-2560x1440.png'}
+	];
 }
