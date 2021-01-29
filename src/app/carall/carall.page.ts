fe6a067bb93c64e6468e3f5de88b93031ff2ad63
obiwan-overflow <kittipopp@hotmail.co.th>
2021-01-29 10:50:32 +0700

    update home

--- src/app/carall/carall.page.ts
+++ src/app/carall/carall.page.ts
@@ -9,6 +9,15 @@ export class CarallPage implements OnInit {
 
   constructor() { }
 
+  cars = [
+  	{car: 'Jazz 01'},
+  	{car: 'Jazz 02'},
+  	{car: 'Jazz 03'},
+  	{car: 'Jazz 04'},
+  	{car: 'Jazz 05'},
+  	{car: 'Jazz 06'},
+  ]
+
   ngOnInit() {
   }
 
